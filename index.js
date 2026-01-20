991d1f75390034e7b88aaf7fa413d2d0b6e8035c
zaidkhatri-dev <zaid.abdulgani.khatri@gmail.com>
2026-01-20 18:16:19 +0530

    fix: fix cors bug

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://gdg-frontend-tau.vercel.app/"],
+    origin: ["http://localhost:5173", "https://gdg-frontend-tau.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://gdg-frontend-tau.vercel.app/"],
+    origin: ["http://localhost:5173", "https://gdg-frontend-tau.vercel.app"],
     methods: ["GET", "POST"]
   }),
 );
